732d22ac7ff41bd870f98f22a533b5a035f3ed77
Shubhangi Pandey <shubhangi.pandey08@gmail.com>
2018-07-07 22:35:04 +0530

    Update guess_game.js

--- guess_game.js
+++ guess_game.js
@@ -1,19 +1,36 @@
-var number = 4;
+var number = Math.floor(Math.random() * 100);
 var guess;
 var limit = 5;
 var won =  false;
+var guesses = [];
 
-for (i = 1; i <= limit; i++){
-    guess = prompt("Guess a number");
-    if(guess == number){
+for (i = 1; i <= limit; i++)
+{
+    guess = parseInt(prompt("Guess a number"));
+    if(guess == number)
+    {
         document.write("Correct! You won.");
         won = true;
         break;
     }
-    else{
-        alert("incorrect. \nTries remaining: " + (limit - i));
+    else
+    {
+        guesses[i] = guess;
+        alert("incorrect. \nYou have guessed: " + guesses.toString() + "\nTries remaining: " + (limit - i));
     }
 }
-if(!won){
-    document.write("Sorry, you ran out of tries. Game over.");
+if(!won)
+{
+    document.write("Sorry, you ran out of tries. Game over. <br> The correct number was: " + number);
+}
+function isPreviousGuess()
+{
+    for(i = 0; i < guesses.length; i++)
+    {
+        if(guesses[i] = guess)
+        {
+            return true;
+        }
+    }
+    return false;
 }
