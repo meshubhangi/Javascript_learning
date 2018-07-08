2855fa8bf012f4ec4ba4eb4c049698bb1986c05d
meshubhangi <shubhangi.pandey08@gmail.com>
2018-07-08 14:51:46 +0530

    changes

--- guess_game.js
+++ guess_game.js
@@ -4,33 +4,22 @@ var limit = 5;
 var won =  false;
 var guesses = [];
 
-for (i = 1; i <= limit; i++)
-{
-    guess = parseInt(prompt("Guess a number"));
-    if(guess == number)
-    {
+// game control loop - iterates for each try
+for (var i = 1; i <= limit; i++){
+    // prompt user for their guess
+    do{
+        guess = parseInt(prompt("Guess a number"));
+    } while(isNaN(guess) || isPreviousGuess(guess));
+    
+    // if correct: let the user know they won
+    if(guess == number){
         document.write("Correct! You won.");
         won = true;
         break;
     }
-    else
-    {
+    // if incorrect: add number to guesses array
+    else{
         guesses[i] = guess;
         alert("incorrect. \nYou have guessed: " + guesses.toString() + "\nTries remaining: " + (limit - i));
     }
-}
-if(!won)
-{
-    document.write("Sorry, you ran out of tries. Game over. <br> The correct number was: " + number);
-}
-function isPreviousGuess()
-{
-    for(i = 0; i < guesses.length; i++)
-    {
-        if(guesses[i] = guess)
-        {
-            return true;
-        }
-    }
-    return false;
 }
