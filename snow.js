d567409ca64671284d66227cd9b54ca7291a2e03
meshubhangi <shubhangi.pandey08@gmail.com>
2018-08-03 21:24:59 +0530

    dd

--- snow.js
+++ snow.js
@@ -1,37 +1,12 @@
-<!-- this script got from www.javascriptfreecode.com-Coded by: Krishna Eydatoula -->
 <script>
-// Set the number of snowflakes (more than 30 - 40 not recommended)
 var snowmax=35
-
-// Set the colors for the snow. Add as many colors as you like
 var snowcolor=new Array("#aaaacc","#ddddFF","#ccccDD")
-
-// Set the fonts, that create the snowflakes. Add as many fonts as you like
 var snowtype=new Array("Arial Black","Arial Narrow","Times","Comic Sans MS")
-
-// Set the letter that creates your snowflake (recommended:*)
 var snowletter="*"
-
-// Set the speed of sinking (recommended values range from 0.3 to 2)
 var sinkspeed=0.6
-
-// Set the maximal-size of your snowflaxes
 var snowmaxsize=22
-
-// Set the minimal-size of your snowflaxes
 var snowminsize=8
-
-// Set the snowing-zone
-// Set 1 for all-over-snowing, set 2 for left-side-snowing 
-// Set 3 for center-snowing, set 4 for right-side-snowing
 var snowingzone=3
-
-///////////////////////////////////////////////////////////////////////////
-// CONFIGURATION ENDS HERE
-///////////////////////////////////////////////////////////////////////////
-
-
-// Do not edit below this line
 var snow=new Array()
 var marginbottom
 var marginright
