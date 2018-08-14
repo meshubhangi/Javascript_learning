91bcd52522fe2d8d66ad6db3b0c3fff90a6506f7
meshubhangi <shubhangi.pandey08@gmail.com>
2018-08-14 10:45:00 +0530

    updates

--- guess_game.js
+++ guess_game.js
@@ -11,7 +11,6 @@ for (i = 1; i <= limit; i++){
         won = true;
         break;
     }
-    // if incorrect: add number to guesses array
     else{
         guesses[i] = guess;
         alert("incorrect. \nYou have guessed: " + guesses.toString() + "\nTries remaining: " + (limit - i));
