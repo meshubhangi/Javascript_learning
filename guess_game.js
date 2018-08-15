5fa3429d1e666058edd2def119bfc623bb5d91cc
meshubhangi <shubhangi.pandey08@gmail.com>
2018-08-15 23:43:00 +0530

    updates

--- guess_game.js
+++ guess_game.js
@@ -5,4 +5,18 @@ var won =  false;
 var guesses = [];
 
 for (i = 1; i <= limit; i++){
-    guess = print
+    guess = prompt("Guess a number");
+    if(guess == number){
+        document.write("Correct! You won.");
+        won = true;
+        break;
+    }
+
+    else{
+        guesses[i] = guess;
+        alert("incorrect. \nYou have guessed: " + guesses.toString() + "\nTries remaining: " + (limit - i));
+    }
+}
+if(!won){
+    document.write("Sorry, you ran out of tries. Game over.");
+}
